d0b07af21eb00a9bd40b2da5b611c5f9b1f221c6
Dmytro Teren <leo20091986@gmail.com>
2025-03-29 11:24:08 +0200

    add timer styles

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,14 +1,16 @@
 
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const dateInput = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector(".btn-start");
 const timerField = document.querySelectorAll(".value");
 
 startBtn.disabled = true;
-
 let userSelectDate = 0;
+
 flatpickr(dateInput, {
     enableTime: true,
     time_24hr: true,
@@ -17,8 +19,15 @@ flatpickr(dateInput, {
     onClose(selectedDates) {
         const currentDate = Date.now();
         if (selectedDates[0] <= currentDate) {
-            alert("Please choose a date in the future");
-            return
+            iziToast.show({
+                title: 'Error',
+                titleColor: '#ffffff',
+                message: 'Please choose a date in the future',
+                messageColor: '#ffffff',
+                color: '#ef4040',
+                position: 'topRight'
+            });
+            return;
         }
         startBtn.disabled = false;
         userSelectDate = +new Date(selectedDates[0]);
@@ -31,33 +40,35 @@ function handleStart() {
     startBtn.disabled = true;
     dateInput.disabled = true;
     
-    const timer = setInterval(() => {
+    const timerId = setInterval(() => {     
         const currentDate = Date.now();
         const deltaTime = userSelectDate - currentDate;
+        if (deltaTime < 0) {
+            clearInterval(timerId);
+            dateInput.disabled = false;
+            return;
+        };
         const deadLine = convertMs(deltaTime);
-        initTimer(deadLine);
+        updateTimer(deadLine, [...timerField]);
     }, 1000);
-
-}
+};
 
 function convertMs(deltaTime) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-    const days = Math.floor(deltaTime / day);
-  
+
+    const days = Math.floor(deltaTime / day);  
     const hours = Math.floor((deltaTime % day) / hour);
     const minutes = Math.floor(((deltaTime % day) % hour) / minute);
     const seconds = Math.floor((((deltaTime % day) % hour) % minute) / second);
-    console.log({ days, hours, minutes, seconds });
     return { days, hours, minutes, seconds };
-}
+};
 
-function initTimer({ days, hours, minutes, seconds }) {
-    const fields = [...timerField];
-    fields[0].textContent = days;
-    fields[1].textContent = hours;
-    fields[2].textContent = minutes;
-    fields[3].textContent = seconds;
-}
+function updateTimer({ days, hours, minutes, seconds }, [fieldDays, fieldHours, fieldMinutes, fieldSeconds]) {
+    fieldDays.textContent = String(days).padStart(2, "0");
+    fieldHours.textContent = String(hours).padStart(2, "0");
+    fieldMinutes.textContent = String(minutes).padStart(2, "0");
+    fieldSeconds.textContent = String(seconds).padStart(2, "0");
+};
